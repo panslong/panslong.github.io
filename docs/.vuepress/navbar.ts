2fa753800f74f0edef5c5e6ac58b4fe5587ccc30
panshuilong <1063589459@qq.com>
2023-12-12 09:57:03 +0800

    first commit

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,197 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const navbarConfig = navbar([
+  "/",
+  // "/home",
+    {
+    text: "Java",
+    icon: "java",
+    prefix: "/Java/",
+    children: [
+      {
+        text: "Java基础",
+        link: "basics/"
+      },
+      {
+        text: "JVM",
+        link: "JVM/"
+      },
+    ],
+  },
+  {
+    text: "数据库",
+    icon: "mysql",
+    prefix: "/database/",
+    children: [
+      {
+        text: "MySQL",
+        link: "MySQL/"
+      },
+      {
+        text: "Redis",
+        link: "Redis/"
+      },
+    ],
+  },
+  {
+    text: "框架",
+    icon: "frame",
+    prefix: "/framework/",
+    children: [
+      {
+        text: "Spring",
+        link: "Spring/"
+      },
+      {
+        text: "SpringBoot",
+        link: "SpringBoot/"
+      },
+      {
+        text: "Mybatis",
+        link: "Mybatis/"
+      }
+    ],
+  },
+  {
+    text: "分布式",
+    icon: "any",
+    prefix: "/distributed/",
+    children: [
+      {
+        text: "RabbitMQ",
+        link: "RabbitMQ/"
+      },
+      {
+        text: "SpringBoot",
+        link: "SpringBoot/"
+      },
+      {
+        text: "分布式系统",
+        link: "distributed/"
+      }
+    ],
+  },
+  // {
+  //   text: "后端",
+  //   icon: "rank",
+  //   prefix: "/basics/",
+  //   children: [
+  //     {
+  //       text: "算法",
+  //       icon: "rank",
+  //       link: "algorithm/"
+  //     },
+  //     {
+  //       text: "MySQL数据库",
+  //       icon: "mysql",
+  //       link: "MySQL/",
+  //     },
+  //     {
+  //       text: "设计模式",
+  //       icon: "repair",
+  //       link: "design-patterns/",
+  //     },
+  //     {
+  //       text: "面向对象",
+  //       icon: "people",
+  //       link: "OOP/OOP",
+  //     },
+  //     {
+  //       text: "设计思想和原则",
+  //       icon: "people",
+  //       link: "design-principles/S",
+  //     },
+  //     {
+  //       text: "分布式",
+  //       icon: "snow",
+  //       link: "distribute/CAP&BASE",
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "语言",
+  //   icon: "language",
+  //   prefix: "/language/",
+  //   children: [
+  //     {
+  //       text: "Java",
+  //       icon: "java",
+  //       link: "Java/"
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "工具",
+  //   icon: "tool",
+  //   prefix: "/tools/",
+  //   children: [
+  //     {
+  //       text: "git",
+  //       icon: "git",
+  //       link: "git"
+  //     },
+  //     {
+  //       text: "linux命令",
+  //       icon: "linux",
+  //       link: "linux命令"
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "项目",
+  //   icon: "strong",
+  //   prefix: "/project/",
+  //   children: [
+  //     {
+  //       text: "lottery抽奖系统",
+  //       icon: "group",
+  //       link: "lottery/lottery-design-patterns"
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "面向招聘",
+  //   icon: "strong",
+  //   prefix: "/recruitment/",
+  //   children: [
+  //     {
+  //       text: "校招",
+  //       icon: "group",
+  //       link: "campus/tipsFromBYRForum"
+  //     },
+  //     {
+  //       text: "社招",
+  //       icon: "mysql",
+  //       link: "/",
+  //     }
+  //   ],
+  // },
+  {
+    text:"导航",
+    link: "/navigation/",
+    icon: "navigation"
+  },
+  // {
+  //   text:"时间轴",
+  //   link: "/timeline/",
+  //   icon: "time"
+  // },
+  {
+    text: "关于",
+    icon: "info",
+    prefix: "/about/",
+    children: [
+      // {
+      //   text: "简历",
+      //   icon: "blog",
+      //   link: "cv",
+      // },
+      {
+        text: "关于本站",
+        icon: "info",
+        link: "guide",
+      }
+    ],
+  }
+
+]);
---
+++ docs/navigation/README.md
@@ -0,0 +1,36 @@
+---
+title: 好用的网址导航
+date: 2022-03-01
+sidebar: 'auto'
+isTimeLine: false
+isShowComment: true
+---
+
+## 常用推荐
+
+[一元机场](https://xn--4gq62f52gdss.com/)		[鸠摩搜索](https://www.jiumodiary.com/)		[在线思维导图ProcessOn](https://www.processon.com/)		[在线绘图draw.io](https://draw.io/)
+
+## IT工具箱
+
+[在线GC日志分析](https://gceasy.io/)		[在线加密解密](http://tool.chacuo.net/cryptdes)
+
+## 工具箱
+
+[在线工具箱集合](https://tool.lu/)		[PDF在线转换工具](https://www.pdf365.cn/)		[在线PDF转WORD](https://www.alltoall.net/)		[二维码生成器](https://cli.im/)		
+
+## 博客论坛
+
+[CSDN](https://www.csdn.net/)		[博客园](https://www.cnblogs.com/)		[简书](https://www.jianshu.com/)		[掘金](https://juejin.im/)		[segmentfault](https://segmentfault.com/)
+
+## 学习教程
+
+[哔哩哔哩](https://www.bilibili.com/)		[腾讯课堂](https://ke.qq.com/)		[中国大学 MOOC](https://www.icourse163.org/)		[极客学院](https://www.jikexueyuan.com/)		[黑马程序员](http://www.itheima.com/)		[尚硅谷](http://www.atguigu.com/)
+
+## 图标素材
+
+[阿里巴巴矢量图标](http://www.iconfont.cn/)
+
+## 影视网站
+
+[美剧天堂](https://www.meijutt.tw/)		[66影视](https://www.66yingshi.com/)		[影猫电影](https://www.mvcat.com/)
+
